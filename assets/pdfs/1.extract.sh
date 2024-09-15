0e901a05c4a828a1548c9d1f6152a7b9d8b0c746
mcc-us <matt.curcio.us@gmail.com>
2024-09-14 23:40:26 -0400

    NEW 1.pdf2table.md & script

--- assets/pdfs/1.extract.sh
+++ assets/pdfs/1.extract.sh
@@ -4,21 +4,28 @@
 touch 1.pdf2table.md
 
 # Append table header
+echo "---" >> 1.pdf2table.md
+echo "layout: post" >> 1.pdf2table.md
+echo "title: IRS Docs and Subject Table" >> 1.pdf2table.md
+echo "---" >> 1.pdf2table.md
+echo " " >> 1.pdf2table.md
 echo "|Document No.|Title|" >> 1.pdf2table.md
-echo "|---|---|" >> 1.pdf2table.md
+echo "|:-|:-|" >> 1.pdf2table.md
+
+/irs.ea/assets/pdfs/f706.pdf
 
 # Loop through all PDF files
 i=0
 for filename in *.pdf; do
   # Extract filename and title
   name="${filename%.*}"
-  title=$(pdfinfo "$filename" | grep Title: | cut -d: -f2- | tr -d '\n\r')
+  title=$(pdfinfo "$filename" | grep "Subject:")
 
   # Create table row
   row="|"
-  row+="$((i + 1))"  # Document number (starting from 1)
-  row+="|<a href=\"$filename\" download>"  # Link with download attribute
-  row+="$title</a>|"
+  row+="$name"  # file name no ext
+  row+="|<a href=/irs.ea/assets/pdfs/\"$filename\" download> "  # Link download attribute
+  row+="$title </a>|"
 
   # Append row to the table
   echo "$row" >> 1.pdf2table.md
