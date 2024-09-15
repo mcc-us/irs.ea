6983b27b414b74355f877ca90e325f4be23a36a0
mcc-us <matt.curcio.us@gmail.com>
2024-09-15 15:12:48 -0400

    mods

---
+++ assets/pdfs/1.sort.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+touch 1.names.txt
+
+# Get a list of all PDF files in the current directory
+files=(*.pdf)
+
+# Remove the first character and store the results in a new array
+new_files=()
+for file in "${files[@]}"; do
+  new_files+=("${file:1}")
+done
+
+# Remove the ".pdf" extension and store the results in another array
+final_files=()
+for file in "${new_files[@]}"; do
+  final_files+=("${file%.pdf}")
+done
+
+# Sort the final list in ascending order
+final_files=($(printf "%s\n" "${final_files[@]}" | sort -n))
+
+# Print the sorted list to a new text file
+printf "%s\n" "${final_files[@]}" > names.txt
